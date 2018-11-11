71bb666cba65b20512ec4d631dffc5b20d6ba9bf
Anirudha <Anirudha@PC2>
2018-11-11 12:57:28 +0530

    multplication class and its caller

---
+++ J250/src/com/ani/gt250/basicstructure/MultiplicationCLS.java
@@ -0,0 +1,13 @@
+package com.ani.gt250.basicstructure;
+
+public class MultiplicationCLS 
+{
+	void printtable(int val)
+	{
+		for(int i = 0; i <= 10; i++)
+		{
+			System.out.printf("%d * %d = %d", val, i, val * i).println();
+		}
+	}
+
+}
---
+++ J250/src/com/ani/gt250/basicstructure/Multiplication_Caller.java
@@ -0,0 +1,12 @@
+package com.ani.gt250.basicstructure;
+
+public class Multiplication_Caller {
+
+	public static void main(String[] args) 
+	{
+		MultiplicationCLS multtable6 = new MultiplicationCLS();
+		multtable6.printtable(6);
+		
+	}
+
+}
