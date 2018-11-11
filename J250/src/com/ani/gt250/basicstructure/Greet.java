04ac43579e0e265a014252a2741a6733889574f3
Anirudha <Anirudha@PC2>
2018-11-11 12:48:59 +0530

    created a package

--- J250/src/com/ani/gt250/basicstructure/Greet.java
+++ J250/src/com/ani/gt250/basicstructure/Greet.java
@@ -1,9 +1,11 @@
+package com.ani.gt250.basicstructure;
 
 public class Greet {
 
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello GIT");
+		System.out.printf("%d", 5);
 
 	}
 
