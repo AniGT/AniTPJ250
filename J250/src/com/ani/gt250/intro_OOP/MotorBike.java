2d7f4eeaa272d11cb76bbcb342733d4854ab16c4
Anirudha <Anirudha@PC2>
2018-11-25 10:47:39 +0530

    Completed 7 section into to OOP

--- J250/src/com/ani/gt250/intro_OOP/MotorBike.java
+++ J250/src/com/ani/gt250/intro_OOP/MotorBike.java
@@ -7,17 +7,51 @@ public class MotorBike
 	
 	
 	// Behaviour
-	void setSpeed(int speed) 
-	{
-		this.speed = speed;
-		
-		System.out.println("Input speed value "+ speed);
-		System.out.println("Class speed value "+ this.speed);
-	}
 	
 	void start()
 	{
 		System.out.println("Bike Started");
 	}
+
+	// Genereated by eclipse
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		if (speed > 0)
+		this.speed = speed;
+	}
+	
+	public void increaseSpeed(int howMuch)
+	{
+		//this.speed += howMuch;
+		setSpeed((this.speed + howMuch));
+	}
+	
+	public void decreaseSpeed(int howMuch)
+	{
+		if(this.speed - howMuch > 0)
+			setSpeed((this.speed - howMuch));
+		//this.speed -= howMuch;
+	}	 
+	
+	// Contructor - for object - Method Gets called after creating an object
+	// Default constructor
+	MotorBike()			// Notice same method name as class and no return type, can have input arg
+	{	
+		//this.speed = 1;   // initialized the speed
+		// OR 
+		this(1);			// Calling another constructor method with a parameter
+		System.out.println("Bike Started");
+	}
+	
+	MotorBike(int a)			// Notice same method name as class and no return type, can have input arg
+	{	
+		this.speed = a;   // initialized the speed
+		
+
+		System.out.println("Bike Started");
+	}	
 	
 }
--- J250/src/com/ani/gt250/intro_OOP/MotorBikeRunner.java
+++ J250/src/com/ani/gt250/intro_OOP/MotorBikeRunner.java
@@ -5,11 +5,13 @@ public class MotorBikeRunner {
 	public static void main(String[] args) 
 	{
 		MotorBike jawa = new MotorBike();
-		MotorBike honda = new MotorBike();
+		MotorBike honda = new MotorBike(5);
+		System.out.println("Speed retrieved from getter for Jawa  after created obt - def constructor " + jawa.getSpeed());
+		System.out.println("Speed retrieved from getter for Honda  after created obt - manual constructor " + honda.getSpeed());
 		
-		jawa.start();
-		honda.start();
-		
+		//jawa.start();
+		//honda.start();
+		 
 	//	jawa.speed = 45; // Not a good practice to directly access instance varaible
 	//	honda.speed = 40; // of a class, it should be done with the methods of that class - Encapsulation
 		
@@ -18,6 +20,17 @@ public class MotorBikeRunner {
 		
 		jawa.setSpeed(15);
 		jawa.setSpeed(10);
+		//jawa.setSpeed(-100);		// Logic to handle such case has to be in class - Encapsulation
+		
+		System.out.println("Speed retrieved from getter for Jawa " + jawa.getSpeed());
+		
+		jawa.increaseSpeed(30);
+		
+		System.out.println("Speed retrieved from getter for Jawa  after increasing " + jawa.getSpeed());
+		
+		jawa.decreaseSpeed(20);
+		
+		System.out.println("Speed retrieved from getter for Jawa  after decreasing " + jawa.getSpeed());
 	}
 
 }
